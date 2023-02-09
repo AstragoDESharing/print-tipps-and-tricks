bd4a78fd272d5aa877ad95da298cb91418c716f3
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-02-09 19:49:00 +0100

    -

--- src/main.py
+++ src/main.py
@@ -3,4 +3,18 @@ from rich import print
 
 # === Main Function ===
 def main():
-    print("Hello World! :wave:")
+    text = "AstragoDE"
+
+    print(f"{text:.<20}")
+    print(f"{text:_>20}")
+    print(f"{text:#^20}")
+
+    print("==================================")
+
+    print(f"{text:<20}")
+    print(f"{text:>20}")
+    print(f"{text:^20}")
+
+
+if __name__ == "__main__":
+    main()
